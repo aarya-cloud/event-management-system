75e908f9d09b2ec55dc7dbfb816882047f02af81
Aarya Patankar <patankaraarya803@gmail.com>
2025-06-25 14:01:11 +0530

    Initial commit - project setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => res.send('API Running'));
+app.listen(process.env.PORT || 8000, () => console.log('Server started'));
